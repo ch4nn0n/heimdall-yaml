21f50c958e6618078894e92165f6ed414ac0abc2
Joshua Channon <josh.channon@bitgamelabs.com>
2019-07-20 17:27:51 +0800

    Add ItemSeeder
    - Will seed items from /config/data.yaml

---
+++ database/seeds/ItemSeeder.php
@@ -0,0 +1,31 @@
+<?php
+
+use Illuminate\Database\Seeder;
+use App\Item;
+use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Facades\File;
+
+class ItemSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        DB::table('items')->delete();
+        $data = File::get("/config/data.yaml");
+        $config = yaml_parse($data)['config'];
+        $items = $config['items'];
+
+        foreach ($items as $obj) {
+            Item::create(array(
+                'title' => $obj['title'],
+                'url' => $obj['url'] ?? null,
+                'pinned' => $obj['pinned'] ?? null,
+                'colour' => $obj['colour'] ?? null,
+            ));
+        }
+    }
+}
